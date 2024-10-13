3e6180706dccc89299a867d6bb4650e852cc5816
rijesh7679 <rijeshrj2001@gmail.com>
2024-10-13 22:45:22 +0530

    Class and Objects

---
+++ FanInfo.java
@@ -0,0 +1,123 @@
+public class FanInfo {
+    public static void main(String[] args) {
+        Fan fan1 = new Fan();
+        fan1.brand = "Havells";
+        fan1.price = 2500;
+        fan1.model = "Crompton Aura";
+        fan1.speed = 3;
+        fan1.type = "Ceiling";
+        fan1.display();
+        fan1.isOn();
+        fan1.isOff();
+        fan1.setSpeed(4);
+        System.out.println("");
+
+        Fan fan2 = new Fan();
+        fan2.brand = "Crompton";
+        fan2.price = 2200;
+        fan2.model = "High Speed";
+        fan2.speed = 5;
+        fan2.type = "Table";
+        fan2.display();
+        fan2.isOn();
+        fan2.isOff();
+        fan2.setSpeed(2);
+        System.out.println("");
+
+        Fan fan3 = new Fan();
+        fan3.brand = "Orient";
+        fan3.price = 1800;
+        fan3.model = "Breeze";
+        fan3.speed = 3;
+        fan3.type = "Ceiling";
+        fan3.display();
+        fan3.isOn();
+        fan3.isOff();
+        fan3.setSpeed(5);
+        System.out.println("");
+
+        Fan fan4 = new Fan();
+        fan4.brand = "USHA";
+        fan4.price = 1500;
+        fan4.model = "Maxx Air";
+        fan4.speed = 2;
+        fan4.type = "Wall";
+        fan4.display();
+        fan4.isOn();
+        fan4.isOff();
+        fan4.setSpeed(1);
+        System.out.println("");
+
+        Fan fan5 = new Fan();
+        fan5.brand = "Bajaj";
+        fan5.price = 1900;
+        fan5.model = "Dynamo";
+        fan5.speed = 4;
+        fan5.type = "Table";
+        fan5.display();
+        fan5.isOn();
+        fan5.isOff();
+        fan5.setSpeed(3);
+        System.out.println("");
+
+        Fan fan6 = new Fan();
+        fan6.brand = "Luminous";
+        fan6.price = 2300;
+        fan6.model = "Stealth";
+        fan6.speed = 5;
+        fan6.type = "Ceiling";
+        fan6.display();
+        fan6.isOn();
+        fan6.isOff();
+        fan6.setSpeed(4);
+        System.out.println("");
+
+        Fan fan7 = new Fan();
+        fan7.brand = "Maharaja";
+        fan7.price = 1600;
+        fan7.model = "Blaze";
+        fan7.speed = 3;
+        fan7.type = "Table";
+        fan7.display();
+        fan7.isOn();
+        fan7.isOff();
+        fan7.setSpeed(2);
+        System.out.println("");
+
+        Fan fan8 = new Fan();
+        fan8.brand = "Havells";
+        fan8.price = 2700;
+        fan8.model = "Vento";
+        fan8.speed = 5;
+        fan8.type = "Ceiling";
+        fan8.display();
+        fan8.isOn();
+        fan8.isOff();
+        fan8.setSpeed(1);
+        System.out.println("");
+
+        Fan fan9 = new Fan();
+        fan9.brand = "Orient";
+        fan9.price = 2000;
+        fan9.model = "Ultra";
+        fan9.speed = 4;
+        fan9.type = "Wall";
+        fan9.display();
+        fan9.isOn();
+        fan9.isOff();
+        fan9.setSpeed(3);
+        System.out.println("");
+
+        Fan fan10 = new Fan();
+        fan10.brand = "Crompton";
+        fan10.price = 2400;
+        fan10.model = "Aura";
+        fan10.speed = 2;
+        fan10.type = "Ceiling";
+        fan10.display();
+        fan10.isOn();
+        fan10.isOff();
+        fan10.setSpeed(5);
+        System.out.println("");
+}
+}
