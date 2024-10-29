a2ac4e1c7ed97912c1cab0eab9f5c467cad9bc2c
rijesh7679 <rijeshrj2001@gmail.com>
2024-10-29 10:10:34 +0530

    Constructor chaining

---
+++ College.java
@@ -0,0 +1,8 @@
+public class College
+{
+	public static void main(String[] args)
+	{
+	CollegeDetails collegeDetails=new CollegeDetails(1,"Shreedevi","manglore","8113822784","571634");
+	collegeDetails.printDetails();
+	}
+}
