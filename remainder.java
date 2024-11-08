003c300882e8487d3aca90091db01d2369b89c96
rijesh7679 <135805304+rijesh7679@users.noreply.github.com>
2024-11-08 12:56:23 +0530

    Add files via upload

---
+++ remainder.java
@@ -0,0 +1,18 @@
+import java.util.scanner;
+{
+public class remainder
+{
+	public static void main(String[] args)
+	{
+		int A,B;
+		Scanner scanner = new Scanner (System.in)
+		System.out.println("eneter the integer A-");
+		int A = scanner.nextInt();
+		System.out.println("enter the integer B-");
+		int B = scanner.nextInt();
+
+		int remainder= A%B;
+		System.out.println("the remainder is :");
+
+	}}
+}
